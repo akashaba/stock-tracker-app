cb050369a9c7e6f79531249a56aa61a6bf2ae935
Brian Akashaba <cla823@legmt.gov>
2025-09-29 12:58:25 -0600

    Implemented Home page by adding widgets from trading view

--- components/TradingViewWidgets.tsx
+++ components/TradingViewWidgets.tsx
@@ -19,7 +19,7 @@ const TradingViewWidget =({title, scriptUrl, config, height=600, className}: Tra
     return (
         <div className="w-full">
             {title && <h3 className="font-semibold text-2xl text-gray-100 mb-5">{title}</h3>}
-            <div className={cn('tradingview-widget-container', className)}ref={container}>
+            <div className={cn('tradingview-widget-container', className)} ref={container}>
                 <div className="tradingview-widget-container__widget" style={{ height, width: "100%" }}/>
                 {/*<div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener nofollow" target="_blank"><span className="blue-text">AAPL stock chart</span></a><span className="trademark"> by TradingView</span></div>*/}
             </div>
