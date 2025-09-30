b2436009e2e7b7daeec89b84b8eaedb0f0095873
Brian Akashaba <cla823@legmt.gov>
2025-09-30 08:54:37 -0600

    Added Sign up page

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,116 @@
+'use client'
+import {SubmitHandler, useForm} from "react-hook-form";
+import {Button} from "@/components/ui/button";
+import InputField from "@/components/forms/inputfield";
+import SelectField from "@/components/forms/SelectField";
+import {INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS} from "@/lib/constants";
+
+import countryList from 'react-select-country-list'
+import {useMemo} from "react";
+import {CountrySelectField} from "@/components/forms/CountrySelectField";
+
+const SignUp = () => {
+    const COUNTRY_LIST = useMemo(() => countryList().getData(), [])
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors, isSubmitting },
+    } = useForm<SignUpFormData>({
+        defaultValues:{
+            fullName:'',
+            email:'',
+            password:'',
+            country:'US',
+            investmentGoals:'Growth',
+            riskTolerance: 'Medium',
+            preferredIndustry:'Technology',
+        },
+        mode: 'onBlur'
+    })
+
+    const onSubmit = async (data: SignUpFormData) => {
+        try {
+            console.log(data)
+        }catch(error) {
+            console.log(error)
+        }
+    }
+    return (
+        <>
+            <h1 className="form-title">Sign Up & Personalize</h1>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+            {/*    INPUTS*/}
+                <InputField
+                name="fullName"
+                label="Full Name"
+                type="text"
+                placeholder="Full Name"
+                register={register}
+                error={errors.fullName}
+                validation={{required:'Name required', minLength:2}}
+                />
+                <InputField
+                    name="email"
+                    label="Email"
+                    type="text"
+                    placeholder="Enter email"
+                    register={register}
+                    error={errors.email}
+                    validation={{required:'Email is required', pattern:/^\w+@\w+\.\w+$/, message:'Invalid email address' }}
+                />
+                <InputField
+                    name="password"
+                    label="Password"
+                    type="password"
+                    placeholder="Enter password"
+                    register={register}
+                    error={errors.password}
+                    validation={{required:'Password is required', minLength:8}}
+                />
+                <CountrySelectField
+                    name="country"
+                    label="Country"
+                    control={control}
+                    error={errors.country}
+                    required
+                />
+
+                <SelectField
+                    name="investmentGoals"
+                    label="Investment Goals"
+                    placeholder="Select your investment goal"
+                    options={INVESTMENT_GOALS}
+                    control={control}
+                    error={errors.investmentGoals}
+                    required
+                />
+                <SelectField
+                    name="riskTolerance"
+                    label="Risk Tolerance"
+                    placeholder="Select your risk level"
+                    options={RISK_TOLERANCE_OPTIONS}
+                    control={control}
+                    error={errors.riskTolerance}
+                    required
+                />
+                <SelectField
+                    name="preferredIndustry"
+                    label="Preferred Industry"
+                    placeholder="Select your preferred Industry"
+                    options={PREFERRED_INDUSTRIES}
+                    control={control}
+                    error={errors.preferredIndustry}
+                    required
+                />
+
+                <Button type="submit" disabled={isSubmitting} className="yellow-btn w-full mt-5">
+                    {isSubmitting ? "Creating Account" : "Start Your Investing Journey"}
+                </Button>
+            </form>
+        </>
+    )
+}
+export default SignUp
